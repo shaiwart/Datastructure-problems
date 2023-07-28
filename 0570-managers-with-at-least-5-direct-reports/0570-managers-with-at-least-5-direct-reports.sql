f253c2d79174288ee6a71d24d293cffbabf4247d
Shailesh Kaiwart <133580233+shaiwart@users.noreply.github.com>
2023-07-28 23:08:48 +0530

    Time: 603 ms (47.40%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,20 @@
-# Write your MySQL query statement below
+# This will give you the IDs of managers who are reported at least 5 times. 
+
 # SELECT managerId
 # FROM Employee e1 
 # GROUP BY managerId
 # HAVING COUNT(managerId) >= 5
 
+# Now use the above select query as Inline view 
+# and join it with the orignal Employee table to get the Name of the manager. 
 
 # SELECT e.name
 # FROM employee e 
-# JOIN (managerTable) m 
+# JOIN (Inline_View) AS m 
 # ON e.id = m.managerId 
 
+
+# Finally replace the Inline_view from the above query with the first query. 
 
 SELECT e.name
 FROM employee e 
